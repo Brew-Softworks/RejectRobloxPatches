af62c3f26589e15e494720d34e1afa8fc3cb3c84
dex <121840104+dex4tw@users.noreply.github.com>
2023-06-22 21:32:31 -0400

    testing

--- Bypass.lua
+++ Bypass.lua
@@ -1,96 +1,51 @@
--- RejectCharacterDeletion SEMI-bypassed by ficello
--- private method found 28/02/2023 (Replicate Welds ONLY)
--- make roblox exploiting great again
+--[[
+    RCD Bypass Rewrite
+    original by @ficello
+    rewritten by @dex4tw
+    (on github) - prototype 1
+]]--
 
--- weld will be saved in a client sided part called "Your username Protected Welds", you'll have again full control of ur welds
+-------------------------------------------------[ Variables & Functions ]-------------------------------------------------
+local plr = game:GetService("Players").LocalPlayer
+local character = plr.Character
+local uis = game:GetService("UserInputService")
 
-function Attachments(P0,P1,POS,ROT) -- credit to the guy who made this align function
-    local AlignPosition = Instance.new("AlignPosition", P0)
-    local AlignOrientation = Instance.new("AlignOrientation", P0)
-    local Attachment1 = Instance.new("Attachment", P0)
-    local Attachment2 = Instance.new("Attachment", P1)
-    AlignPosition.Attachment0 = Attachment1
-    AlignOrientation.Attachment0 = Attachment1
-    AlignPosition.Attachment1 = Attachment2
-    AlignOrientation.Attachment1 = Attachment2
-    AlignPosition.Responsiveness = 300
-    AlignPosition.MaxForce = 5e9
-    AlignOrientation.MaxTorque = 5e9
-    AlignOrientation.Responsiveness = 300
-    AlignOrientation.ReactionTorqueEnabled = false
-    Attachment1.Position = POS or Vector3.new(0,0,0)
-    Attachment1.Orientation = ROT or Vector3.new(0,0,0)
-end
+-- Replace HumanoidRootPart --
+local hrpReplacement = Instance.new("Part", character)
+hrpReplacement.CFrame = character.HumanoidRootPart.CFrame
+hrpReplacement.Transparency = 1
+hrpReplacement.Anchored = true
+hrpReplacement.CanCollide = false
 
-function patchWelds()
-    if game:GetService("Players").LocalPlayer.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-        local lp = game.Players.LocalPlayer
-        local char = lp.Character
-        
-        lp.Character = nil -- permadeath
-        lp.Character = char -- bypass
-        
-        local hrp = char:FindFirstChild("HumanoidRootPart") 
-        if hrp == nil then return end 
-        
-        hrp:Destroy()  
-        wait(6)
-        local protectedWelds = Instance.new("Part", workspace)
-        protectedWelds.Name = lp.Name .. " Protected Welds"
-        protectedWelds.Anchored = true
-        protectedWelds.CFrame = char.UpperTorso.CFrame
-        protectedWelds.CanCollide = false
-        protectedWelds.Transparency = 1
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "RejectCharacterDeletion bypass by ficello", 
-            Text  = "You can now reset without any issues", 
-            Icon  = "rbxassetid://17703918" 
-        })
-        wait(3)
-        for k, v in pairs(char:GetChildren()) do
-            if v:IsA("Accessory") then
-                Attachments(v.Handle, protectedWelds, Vector3.new(0, 0, 0), Vector3.new(0, 0, 0))
+-------------------------------------------------[ Patch Welds ]-------------------------------------------------
+plr.Character = nil
+plr.Character = character
+
+local hrp = character:FindFirstChild("HumanoidRootPart") 
+if hrp == nil then return end 
+
+hrp:Destroy() 
+wait(6)
+local protectedWelds = Instance.new("Part", workspace)
+protectedWelds.Name = plr.Name .. " Protected Welds"
+protectedWelds.Anchored = true
+protectedWelds.CFrame = character.Torso.CFrame
+protectedWelds.CanCollide = false
+protectedWelds.Transparency = 1
+wait(3)
+
+-------------------------------------------------[ Netless ]-------------------------------------------------
+coroutine.resume(coroutine.create(function()
+    game:GetService("RunService").Heartbeat:Connect(function()
+        for i,v in next, workspace[game.Players.LocalPlayer.Name]:GetChildren() do
+            if v:IsA("BasePart") then 
+                v.Velocity = Vector3.new(0,-32.5,0)
+            elseif v:IsA("Accessory") then 
+                v.Handle.Velocity = Vector3.new(0,-32.5,0)
             end
         end
-    elseif game:GetService("Players").LocalPlayer.Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
-        local lp = game.Players.LocalPlayer
-        local char = lp.Character
-        
-        lp.Character = nil -- permadeath
-        lp.Character = char -- bypass
-        
-        local hrp = char:FindFirstChild("HumanoidRootPart") 
-        if hrp == nil then return end 
-        
-        hrp:Destroy() 
-        wait(6)
-        local protectedWelds = Instance.new("Part", workspace)
-        protectedWelds.Name = lp.Name .. " Protected Welds"
-        protectedWelds.Anchored = true
-        protectedWelds.CFrame = char.Torso.CFrame
-        protectedWelds.CanCollide = false
-        protectedWelds.Transparency = 1
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "RejectCharacterDeletion bypass by ficello", 
-            Text  = "You can now reset without any issues", 
-            Icon  = "rbxassetid://17703918" 
-        })
-        wait(3)
-        for k, v in pairs(char:GetChildren()) do
-            if v:IsA("Accessory") then
-                Attachments(v.Handle, protectedWelds, Vector3.new(0, 0, 0), Vector3.new(0, 0, 0))
-            end
-        end
-    else
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "RejectCharacterDeletion bypass by ficello", 
-            Text  = "tf rig r u using lmao", 
-            Icon  = "rbxassetid://17703918" 
-        })
-    end
-end
+    end)
+end))
 
-
--- "bypassing" RejectCharacterDeletion
-patchWelds()
--- end
+-------------------------------------------------[ Source ]-------------------------------------------------
+-- your code here
