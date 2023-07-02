1c50fd5d74da84e80b9d9636964a5d7eb19716cf
dex <121840104+dex4tw@users.noreply.github.com>
2023-07-02 16:00:37 -0400

    inventory update

--- Examples/dex-dummy.lua
+++ Examples/dex-dummy.lua
@@ -1,8 +1,14 @@
 --[[
-    Dex Dummy [ NO RCD ]
-        Create a Dummy with your accessories,
-	-- please try to keep this script somewhat "silent"
-	-- i do not want anybody over-using it
+    Dex Dummy [ About ]
+        Create a Dummy with your accessories
+	*       please try to keep this script somewhat "silent"
+	*       i do not want anybody over-using it
+        PS: Too much netless MAY lag you.
+    
+    Dex Dummy [ Controls ]
+    1 - 4 : Inventory Keys
+    E : Enable / Disable Inventory Orbit
+
     @dex4tw - Discord & Roblox
 ]]--
 -- ooo you somehow heard of this repo
@@ -64,15 +70,14 @@ for i,v in pairs(character:GetChildren()) do
     end
 end
 
+if workspace:FindFirstChild("nilly") then workspace.nilly:Destroy() end
 local me = Instance.new("Part", workspace)
+me.Name = "nilly"
 me.Size = Vector3.new(2,500,2)
 me.Anchored = true
 me.CanCollide = false
 me.Transparency = .5
 me.CFrame = character.HumanoidRootPart.CFrame
-character.Humanoid.Died:Connect(function()
-    me:Destroy()
-end)
 
 -------------------------------------------------[ Patch Character ]-------------------------------------------------
 local OldPos = character["HumanoidRootPart"]
@@ -126,7 +131,7 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.Three then
+    elseif key.KeyCode == Enum.KeyCode.Three and not typing then
         if equip == false then
             item = 3
             equip = true
@@ -138,7 +143,7 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.Four then
+    elseif key.KeyCode == Enum.KeyCode.Four and not typing then
         if equip == false then
             item = 4
             equip = true
@@ -150,7 +155,7 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.E then
+    elseif key.KeyCode == Enum.KeyCode.E and not typing then
         if orbit == true then
             orbit = false
         else
@@ -160,22 +165,25 @@ uis.InputBegan:Connect(function(key, typing)
 end)
 
 -------------------------------------------------[ Dummy Control ]-------------------------------------------------
-local orbitRadius = 10 -- Adjust the radius of the orbit
-local orbitSpeed = .5 -- Adjust the speed of the orbit
+local orbitRadius = 15 -- Adjust the radius of the orbit
+local orbitSpeed = .35 -- Adjust the speed of the orbit
+local orbitDistance = 5 -- Adjust the distance of the items from the character
+local angle = 0
+local distanceOffset = 0 -- Very self explanatory...
 
 function orbitItem(item, angle)
-    if orbit == true then
-        local offset = Vector3.new(math.cos(angle) * orbitRadius, 0, math.sin(angle) * orbitRadius)
-        item.Handle.CFrame = character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(offset))
-    else
-        item.Handle.CFrame = character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(0,-15,0))
-    end
+    local offset = Vector3.new(math.cos(angle) * orbitRadius, math.sin(angle) * orbitDistance, math.sin(angle) * orbitRadius)
+    item.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(offset))
 end
 
-local angle = 0
 
 game:GetService("RunService").Heartbeat:Connect(function(deltaTime)
+    -- Update the orbit angle
     angle = angle + (orbitSpeed * deltaTime)
+
+    -- Update the distance offset
+    distanceOffset = distanceOffset + (orbitSpeed * deltaTime)
+    local distance = math.sin(distanceOffset) * orbitDistance
 
     pcall(loadstring("head.Handle.CFrame = character.Head.CFrame"))
     pcall(loadstring('torso.Handle.CFrame = character.Torso.CFrame * CFrame.Angles(0, 17.28, 0)'))
@@ -185,30 +193,33 @@ game:GetService("RunService").Heartbeat:Connect(function(deltaTime)
 
     if equip == false then
         pcall(loadstring('rightarm.Handle.CFrame = character["Right Arm"].CFrame * CFrame.Angles(17.28, 0, 0)'))
+        pcall(loadstring("tools[4].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
+        pcall(loadstring("tools[3].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
+        pcall(loadstring("tools[2].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
+        pcall(loadstring("tools[1].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
+
+        -- Orbit the unequipped items with in-and-out movement
         for i, tool in ipairs(tools) do
             if i ~= item then
-                local toolAngle = angle + (i * (2 * math.pi) / 4)
+                local toolAngle = angle + (i * (2 * math.pi) / 4) -- Distribute the items evenly around the character
                 orbitItem(tool, toolAngle)
             end
         end
     else
         pcall(loadstring('rightarm.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -.5))'))
 
+        -- Orbit the unequipped items without in-and-out movement
         for i, tool in ipairs(tools) do
             if i ~= item then
-                local toolAngle = angle + (i * (2 * math.pi) / 4)
+                local toolAngle = angle + (i * (2 * math.pi) / 4) -- Distribute the items evenly around the character
                 orbitItem(tool, toolAngle)
             end
         end
 
-        if item == 1 then
-            pcall(loadstring("tools[1].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))"))
-        elseif item == 2 then
-            pcall(loadstring("tools[2].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))"))
-        elseif item == 3 then
-            pcall(loadstring("tools[3].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))"))
-        elseif item == 4 then
-            pcall(loadstring("tools[4].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))"))
+        -- Move and rotate the equipped item
+        local equippedTool = tools[item]
+        if equippedTool then
+            equippedTool.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))
         end
     end
 end)
