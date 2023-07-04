41e42b82e273ff330b5c7f994b6014a595593c2f
dex <121840104+dex4tw@users.noreply.github.com>
2023-07-04 07:57:59 -0400

    cframe lerping update

--- Examples/dex-dummy.lua
+++ Examples/dex-dummy.lua
@@ -3,6 +3,7 @@
         Create a Dummy with your accessories
 	*       please try to keep this script somewhat "silent"
 	*       i do not want anybody over-using it
+    *       this is a NOT a special-edition anymore since its on github (enjoy the cframe interpolation)
         PS: Too much netless MAY lag you.
     
     Dex Dummy [ Controls ]
@@ -11,20 +12,30 @@
 
     @dex4tw - Discord & Roblox
 ]]--
--- ooo you somehow heard of this repo
--- congrats on asking for the script
 
 -------------------------------------------------[ Variables & Functions ]-------------------------------------------------
+
+-- Default Variables --
 plr = game:GetService("Players").LocalPlayer
 character = plr.Character
 uis = game:GetService("UserInputService")
 
-torso = character['LongStraightHair']
-head = character['SidePonytail']
-leftarm = character["MessyHair"]
-rightarm = character["Pal Hair"]
-leftleg = character["Pink Hair"]
-rightleg = character["LavanderHair"]
+-- Rig Variables --
+torso = character['LongStraightHair'];torso.Handle.Mesh:Destroy()
+head = character['SidePonytail'];head.Handle.Mesh:Destroy()
+leftarm = character["MessyHair"];leftarm.Handle.Mesh:Destroy()
+rightarm = character["Pal Hair"];rightarm.Handle.Mesh:Destroy()
+leftleg = character["Pink Hair"];leftleg.Handle.Mesh:Destroy()
+rightleg = character["LavanderHair"];rightleg.Handle.Mesh:Destroy()
+orbitRadius = 15
+orbitSpeed = .35
+orbitDistance = 5
+angle = 0
+distanceOffset = 0
+equip = false
+orbit = false
+item = 0
+
 tools = {
     [1] = character['WiseMysticalTree'],
     [2] = character["WeaponAccessory"],
@@ -32,44 +43,37 @@ tools = {
     [4] = character['PointyTowerHat']
 }
 
--------------------------------------------------[ Patch Accessories ]-------------------------------------------------
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dex4tw/RejectCharacterDeletions/main/AcessoryNetless.lua"))()
-for i,v in pairs(character:GetChildren()) do
-    if v:IsA("Accessory") then
-        v.Handle:BreakJoints()
-        v.Handle.CanCollide = true
-        -- Add Attachments & Stuff --
-        local AlignPosition = Instance.new("AlignPosition", v.Handle)
-        local AlignOrientation = Instance.new("AlignOrientation", v.Handle)
-        local Attachment1 = Instance.new("Attachment", v.Handle)
-        local Attachment2 = Instance.new("Attachment", character['HumanoidRootPart'])
-        AlignPosition.Attachment0 = Attachment1
-        AlignOrientation.Attachment0 = Attachment1
-        AlignPosition.Attachment1 = Attachment2
-        AlignOrientation.Attachment1 = Attachment2
-        AlignPosition.Responsiveness = 300
-        AlignPosition.MaxForce = 5e9
-        AlignOrientation.MaxTorque = 5e9
-        AlignOrientation.Responsiveness = 300
-        AlignOrientation.ReactionTorqueEnabled = false
-        Attachment1.Position = torso.Handle.Position
-        Attachment1.Orientation = torso.Handle.Orientation
+-- General Functions --
+function cLerp(part, targetCFrame, duration)
+    return coroutine.wrap(function()
+        local startTime = tick()
+        local startCFrame = part.CFrame
+        while tick() - startTime < duration do
+            local elapsedTime = tick() - startTime
+            local t = elapsedTime / duration
+            local lerpedCFrame = startCFrame:Lerp(targetCFrame, t)
+            local slerpedCFrame = CFrame.new(
+                lerpedCFrame.Position,
+                targetCFrame.Position
+            ):lerp(lerpedCFrame, math.sin(t * math.pi * 0.5))
 
-        -- Add Velocity --
-        game:GetService("RunService").Heartbeat:Connect(function()
-            character['HumanoidRootPart'].Velocity = Vector3.new(0, 35, 0)
-        end)
-
-        if v == head or v == torso or v == leftarm or v == rightarm or v == leftleg or v== rightleg then
-            if v.Handle:FindFirstChild('SpecialMesh') then 
-                v.Handle.SpecialMesh:Destroy()
-            elseif v.Handle:FindFirstChild("Mesh") then
-                v.Handle.Mesh:Destroy()
-            end
+            part.CFrame = slerpedCFrame
+            game:GetService("RunService").Heartbeat:Wait()
         end
+        part.CFrame = targetCFrame
+    end)
+end
+
+function orbitItem(item, angle)
+    if orbit then
+        local offset = Vector3.new(math.cos(angle) * orbitRadius, math.sin(angle) * orbitDistance, math.sin(angle) * orbitRadius)
+        cLerp(item.Handle, character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(offset)), 0.15)()
+    else
+        cLerp(item.Handle, character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(0,-15,0)), 0.15)()
     end
 end
 
+-------------------------------------------------[ Patch Character ]-------------------------------------------------
 if workspace:FindFirstChild("nilly") then workspace.nilly:Destroy() end
 local me = Instance.new("Part", workspace)
 me.Name = "nilly"
@@ -78,9 +82,6 @@ me.Anchored = true
 me.CanCollide = false
 me.Transparency = .5
 me.CFrame = character.HumanoidRootPart.CFrame
-
--------------------------------------------------[ Patch Character ]-------------------------------------------------
-local OldPos = character["HumanoidRootPart"]
 local Seat = Instance.new("Seat")
 local Weld = Instance.new("Weld")
 Seat.Transparency = 1
@@ -95,19 +96,25 @@ Weld.Part0 = Seat
 Weld.Part1 = character["HumanoidRootPart"]
 character["HumanoidRootPart"].Anchored = false
 Seat.CFrame = character["HumanoidRootPart"].CFrame
-
 for i,v in pairs(character:GetChildren()) do
 	if v:IsA("Part") then
 		v.Transparency = 1
 	end
 end
 
--------------------------------------------------[ Dummy Inventory ]-------------------------------------------------
-equip = false
-orbit = true
-item = 0
+
+
+-------------------------------------------------[ Patch Accessories ]-------------------------------------------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dex4tw/RejectCharacterDeletions/main/AcessoryNetless.lua"))()
+for i,v in pairs(character:GetChildren()) do
+    if v:IsA("Accessory") then
+        v.Handle:BreakJoints()
+    end
+end
+
+-------------------------------------------------[ Dummy Controls ]-------------------------------------------------
 uis.InputBegan:Connect(function(key, typing)
-    if key.KeyCode == Enum.KeyCode.One and not typing then
+    if key.KeyCode == Enum.KeyCode.One and not typing then orbit = true wait(.4)
         if equip == false then
             item = 1
             equip = true
@@ -119,7 +126,9 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.Two and not typing then
+        wait(.5)
+        orbit = false
+    elseif key.KeyCode == Enum.KeyCode.Two and not typing then orbit = true wait(.4)
         if equip == false then
             item = 2
             equip = true
@@ -131,7 +140,9 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.Three and not typing then
+        wait(.5)
+        orbit = false
+    elseif key.KeyCode == Enum.KeyCode.Three and not typing then orbit = true wait(.4)
         if equip == false then
             item = 3
             equip = true
@@ -143,7 +154,9 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
-    elseif key.KeyCode == Enum.KeyCode.Four and not typing then
+        wait(.5)
+        orbit = false
+    elseif key.KeyCode == Enum.KeyCode.Four and not typing then orbit = true wait(.4)
         if equip == false then
             item = 4
             equip = true
@@ -155,6 +168,8 @@ uis.InputBegan:Connect(function(key, typing)
             item = 0
             equip = false
         end
+        wait(.5)
+        orbit = false
     elseif key.KeyCode == Enum.KeyCode.E and not typing then
         if orbit == true then
             orbit = false
@@ -164,45 +179,27 @@ uis.InputBegan:Connect(function(key, typing)
     end
 end)
 
--------------------------------------------------[ Dummy Control ]-------------------------------------------------
-local orbitRadius = 15 -- Adjust the radius of the orbit
-local orbitSpeed = .35 -- Adjust the speed of the orbit
-local orbitDistance = 5 -- Adjust the distance of the items from the character
-local angle = 0
-local distanceOffset = 0 -- Very self explanatory...
-
-function orbitItem(item, angle)
-    if orbit then
-        local offset = Vector3.new(math.cos(angle) * orbitRadius, math.sin(angle) * orbitDistance, math.sin(angle) * orbitRadius)
-        item.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(offset))
-    else
-        item.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0,-15,0))
-    end
-end
-
-
+-------------------------------------------------[ CFrame Interpolation ]-------------------------------------------------
 game:GetService("RunService").Heartbeat:Connect(function(deltaTime)
-    -- Update the orbit angle
+    -- Orbit Variables --
     angle = angle + (orbitSpeed * deltaTime)
-
-    -- Update the distance offset
     distanceOffset = distanceOffset + (orbitSpeed * deltaTime)
     local distance = math.sin(distanceOffset) * orbitDistance
 
-    pcall(loadstring("head.Handle.CFrame = character.Head.CFrame"))
-    pcall(loadstring('torso.Handle.CFrame = character.Torso.CFrame * CFrame.Angles(0, 17.28, 0)'))
-    pcall(loadstring('leftarm.Handle.CFrame = character["Left Arm"].CFrame * CFrame.Angles(17.28, 0, 0)'))
-    pcall(loadstring('leftleg.Handle.CFrame = character["Left Leg"].CFrame * CFrame.Angles(17.28, 0, 0)'))
-    pcall(loadstring('rightleg.Handle.CFrame = character["Right Leg"].CFrame * CFrame.Angles(17.28, 0, 0)'))
+    -- Main Body Lerping --
+    cLerp(head.Handle, character.Head.CFrame, 0)()
+    cLerp(torso.Handle, character.Torso.CFrame * CFrame.Angles(0, 17.28, 0), 0)()
+    cLerp(leftarm.Handle, character["Left Arm"].CFrame * CFrame.Angles(17.28, 0, 0), 0)()
+    cLerp(leftleg.Handle, character["Left Leg"].CFrame * CFrame.Angles(17.28, 0, 0), 0)()
+    cLerp(rightleg.Handle, character["Right Leg"].CFrame * CFrame.Angles(17.28, 0, 0), 0)()
 
-    if equip == false then
-        pcall(loadstring('rightarm.Handle.CFrame = character["Right Arm"].CFrame * CFrame.Angles(17.28, 0, 0)'))
-        pcall(loadstring("tools[4].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
-        pcall(loadstring("tools[3].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
-        pcall(loadstring("tools[2].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
-        pcall(loadstring("tools[1].Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0))"))
-
-        -- Orbit the unequipped items with in-and-out movement
+    -- Inventory Lerping --
+    if not equip then
+        cLerp(rightarm.Handle, character["Right Arm"].CFrame * CFrame.Angles(17.28, 0, 0), 0)()
+        cLerp(tools[4].Handle, character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0)), 0.25)()
+        cLerp(tools[3].Handle, character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0)), 0.25)()
+        cLerp(tools[2].Handle, character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0)), 0.25)()
+        cLerp(tools[1].Handle, character.Torso.CFrame:ToWorldSpace(CFrame.new(0, -15, 0)), 0.25)()
         for i, tool in ipairs(tools) do
             if i ~= item then
                 local toolAngle = angle + (i * (2 * math.pi) / 4) -- Distribute the items evenly around the character
@@ -210,20 +207,26 @@ game:GetService("RunService").Heartbeat:Connect(function(deltaTime)
             end
         end
     else
-        pcall(loadstring('rightarm.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -.5))'))
-
-        -- Orbit the unequipped items without in-and-out movement
+        cLerp(rightarm.Handle, character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -.5)), 0)()
         for i, tool in ipairs(tools) do
             if i ~= item then
-                local toolAngle = angle + (i * (2 * math.pi) / 4) -- Distribute the items evenly around the character
+                local toolAngle = angle + (i * (2 * math.pi) / 4)
                 orbitItem(tool, toolAngle)
             end
         end
 
-        -- Move and rotate the equipped item
         local equippedTool = tools[item]
         if equippedTool then
-            equippedTool.Handle.CFrame = character.Torso.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))
+            equipLerp = true
+            if equipLerp then
+                cLerp(equippedTool.Handle, character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2)) * CFrame.Angles(0,0,0), 0.1)()
+                wait(0.35)
+                equipLerp = false
+            else
+                equippedTool.Handle.CFrame = character.HumanoidRootPart.CFrame:ToWorldSpace(CFrame.new(1.4, .5, -2))
+            end
+        else
+            equipLerp = false
         end
     end
 end)
