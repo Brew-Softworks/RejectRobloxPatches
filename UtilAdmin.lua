39d406d7cdf2c15afc3c4990de14ed29177b133d
dex <121840104+dex4tw@users.noreply.github.com>
2023-07-09 17:26:14 -0400

    more info

--- UtilAdmin.lua
+++ UtilAdmin.lua
@@ -4,6 +4,27 @@ Util Admin v1.00
   for the game that it you are playing on )
 If the script doesn't run it either errored
 or the game is not supported. (check console)
+
+TO CANCEL RUNSERVICE LOOPS, RESET. (stops old scripts from running ex: drophats, draghats, hatorbit etc.)
+
+Press ; to focus the command bar
+Commands:
+void - put yourself in the void (prevents bangers and loop tp'ers)
+unvoid - return to where you last were
+trip - trips your player
+sit - sit down
+to (str)[user] - can be shortened user or displayname (teleports to them)
+blockhats - make your hats blocks
+drophats (int)[handle size] - drop your hats
+hatorbit - make your hats orbit you press + or - to change modes
+draghats - drag your hats with your mouse
+nohats - deletes your hats
+rj - rejoin the game
+re - refresh your character
+noclip - clip through things
+clip - dont clip through things
+netless - uses the netless from my github
+
 ]]--
 -- Use on very specific (RejectCharacterDeletions DISABLED) games such as:
 -- https://www.roblox.com/games/4483381587/a-literal-baseplate
